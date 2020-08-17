4b931ed57744969200e4b73f1827a634ea751f50
Justin Workman <xtagon@gmail.com>
2020-08-16 18:30:22 -0700

    Match commands case-insensitively and with or without a bang.

--- lib/chatsnek/chat_handler.ex
+++ lib/chatsnek/chat_handler.ex
@@ -2,16 +2,26 @@ defmodule ChatSnek.ChatHandler do
   use TMI.Handler
   alias ChatSnek.VoteManager
 
+  @directions ["up", "down", "left", "right"]
+
   @impl true
-  def handle_message("!" <> direction, sender, _chat)
-  when direction in ["up", "down", "left", "right"]
-  do
+  def handle_message("!" <> message, sender, chat) do
+    handle_message(message, sender, chat)
+  end
+
+  @impl true
+  def handle_message(message, sender, chat) do
+    message
+    |> String.trim
+    |> String.downcase
+    |> handle_command(sender, chat)
+  end
+
+  def handle_command(direction, sender, _chat) when direction in @directions do
     Logger.info("Chat: #{sender} voted to move #{direction}")
     VoteManager.cast_vote(direction, sender)
   end
 
-  @impl true
-  def handle_message(_message, _sender, _chat) do
-    # Do nothing, just ignore anything that's not a command for us.
-  end
+  # Ignore anything that isn't a valid command
+  def handle_command(_command, _sender, _chat), do: nil
 end
